0ac36e926d14c2eb037c59879e9143efa856519b
Taylan Mayckon <130873668+Nexcauzin@users.noreply.github.com>
2024-05-05 04:34:59 -0300

    V1.2

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ with open('custom_payloads/contratar_plano.json', 'r+', encoding='utf-8') as con
 with open('custom_payloads/cadastro_promocoes.json', 'r+', encoding='utf-8') as cad_num:
     cadastro_promocoes = json.load(cad_num)
 
+with open('custom_payloads/promocoes_ativas.json', 'r+', encoding='utf-8') as ver_prom:
+    ver_promocoes = json.load(ver_prom)
 
 @app.route('/', methods=['POST'])
 def main():
@@ -62,8 +64,25 @@ def main():
     except:
         pass
 
+
+    # Bloco 3 -> Testa se é para ver Promoções Ativas
+    # Depois melhora para um Webhook que puxa a mensagem de algum lugar automaticamente
+    try:
+        for contexto in contextos:
+            parametros = contexto['parameters']
+            # Teste de existência para o parametro promoções
+            promocoes = parametros['Promocoes']
+
+        if data['originalDetectIntentRequest']['source'] == 'telegram':
+            data['fulfillmentText'] = [{"payload": ver_promocoes}]
+
+
+    except:
+        pass
+
     # Descomenta quando quiser o json bruto
     #print(data)
+
 
     return jsonify(data)
 
