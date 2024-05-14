b47e91e2a345e1e4249dd8b433cd9102080b03d7
Taylan Mayckon <130873668+Nexcauzin@users.noreply.github.com>
2024-05-13 23:47:46 -0300

    V1.5 Tentativas de CRON (TELEGRAM)

--- cron/envia_promocoes (TELEGRAM BASE).py
+++ cron/envia_promocoes (TELEGRAM BASE).py
@@ -6,7 +6,10 @@ from telegram import Bot
 
 #token = '6315254600:AAE0XXGHcfV2TsVsbTL-FECvihTKXvNF800'
 token = '7047287612:AAEMimLtSeFAbVsgkY8cmGKnZZhVjon5vik'
-id_usuario = '6758080824'
+id_usuario = '2079298675'
+
+#id meu: 2079298675
+#id de jf: 6758080824
 
 async def send_telegram_message():
     try:
