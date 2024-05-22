e234d508c23bde36fcca3737b86aec838c966b8f
Taylan Mayckon <130873668+Nexcauzin@users.noreply.github.com>
2024-05-22 04:13:58 -0300

    Ajustando as funções assíncronas

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from threading import Thread
 app = Flask(__name__)
 
 # Variáveis para o envio periódico de mensagens:
-# TOKEN do bot Telegram
 envia_promocoes.token_telegram = '7047287612:AAEMimLtSeFAbVsgkY8cmGKnZZhVjon5vik'
 
 # Inicializando o CRON
@@ -24,14 +23,22 @@ def main_route():
     data = request.get_json(silent=True, force=True)
     contextos = data['queryResult']['outputContexts']
 
+    cadastro_whatsapp_feito = False
+    cadastro_telegram_feito = False
+
     # Bloco 1 -> Testa se é para Cadastrar na Lista de Transmissão (WhatsApp)
     try:
         for contexto in contextos:
             parametros = contexto['parameters']
             nome = parametros['nome']
             numero = parametros['numero']
-            print(f'Nome: {nome} | Tel: {numero}')
-            Thread(target=cadastrar_sheets_zap, args=([nome, numero],)).start()
+            if nome and numero:
+                print(f'Nome: {nome} | Tel: {numero}')
+                Thread(target=cadastrar_sheets_zap, args=([nome, numero],)).start()
+                cadastro_whatsapp_feito = True
+        if not cadastro_whatsapp_feito:
+            print('Nenhum cadastro para WhatsApp foi encontrado nos contextos')
+        return jsonify(data)
     except Exception as e:
         print(f"Erro no cadastro do WhatsApp: {e}")
 
@@ -41,16 +48,16 @@ def main_route():
             parametros = contexto['parameters']
             nome = parametros['nome']
             id = data['session'].split('/')[-1]
-            print(f'Nome: {nome} | ID: {id}')
-            Thread(target=cadastrar_sheets_tel, args=([nome, id],)).start()
+            if nome and id:
+                print(f'Nome: {nome} | ID: {id}')
+                Thread(target=cadastrar_sheets_tel, args=([nome, id],)).start()
+                cadastro_telegram_feito = True
+        if not cadastro_telegram_feito:
+            print('Nenhum cadastro para Telegram foi encontrado nos contextos')
     except Exception as e:
         print(f"Erro no cadastro do Telegram: {e}")
 
-    # Descomenta quando quiser o json bruto
-    #print(data)
-
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.debug = False
